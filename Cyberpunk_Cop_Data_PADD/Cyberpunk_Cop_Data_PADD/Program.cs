8ee889291ac0d3cbdeddd90f6345ba4fed72d5bd
skillkill235 <skillkill234@gmail.com>
2019-08-11 20:27:17 -0700

    Created basic files, encounter error

---
+++ Cyberpunk_Cop_Data_PADD/Cyberpunk_Cop_Data_PADD/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Cyberpunk_Cop_Data_PADD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Dev Notes
+             *---------------------------------------------------------
+             * This is a console app for a cyberpunk cop data PADD written in C#
+             * It's basic functions are to:
+             *   -Take user input in form of a random 9 digit alphanumeric code
+             *   -Based on above 9 digit code, generate the following
+             *       -Name
+             *       -Age
+             *       -Expanded Citizen Id
+             *       -Date of Birth (custom date system)
+             *       -Randomly generated finger and retinal prints
+             *       -Wanted Status
+             *       -List of crimes
+             *       -Generate random ticket/fines
+             *   -Display all the information above
+             *   -Print out a ticket with all the above information
+             *   -Prints a seperate ticket for tickets/fines
+             *---------------------------------------------------------
+             */
+
+
+            //Begin Boot Sequence
+            //---------------------------------------------------------
+
+            Console.WriteLine("System Starting, Please Wait....."); // prints the boot loding message
+
+            //add random sleep time here
+
+            Console.WriteLine("Loading Memory...... 64kb");
+            Console.WriteLine("Loading Memory...... 128kb");
+            //add random sleep time here
+            Console.WriteLine("Loading Memory...... 256kb");
+            Console.WriteLine("Loading Memory...... 512kb");
+            //add random sleep time here
+
+            Console.WriteLine("Memory Loaded");
+            Console.WriteLine("Booting OS, Please Wait.....");
+            //add random sleep time here, 30s minimum
+            Console.WriteLine("        /|");
+            Console.WriteLine("       / |");
+            Console.WriteLine("      /  |");
+            Console.WriteLine("     /   |");
+            Console.WriteLine("    /    |");
+            Console.WriteLine("   /     |");
+            Console.WriteLine("  /      |");
+            Console.WriteLine(" /       |");
+            Console.WriteLine("/________|");
+            Console.WriteLine("TRIANGLE CORPS PRESENTS");
+            Console.WriteLine("Right Angle OS");
+            Console.WriteLine("v 0.01.23.453");
+            //end of boot sequence
+            //---------------------------------------------------------
+
+            //begin usage
+
+            Console.WriteLine("Please Enter User ID:");
+            string userId = Console.ReadLine();
+            Console.WriteLine("Please Enter Password:");
+            string = userPassword = Console.ReadLine();
+            Console.WriteLine("Connecting to GlobaNet Services.....");
+            //add random sleep time here
+            Console.WriteLine("Credentials Accepted, Welcome User " + userId + "!");
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}

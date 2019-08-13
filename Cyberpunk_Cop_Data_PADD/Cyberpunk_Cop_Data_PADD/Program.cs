aa0b7b2bc016eb5cf1d579943568cbdcaffb65b8
skillkill235 <skillkill234@gmail.com>
2019-08-13 12:55:17 -0700

    i think i fixed it (for now)

--- Cyberpunk_Cop_Data_PADD/Cyberpunk_Cop_Data_PADD/Program.cs
+++ Cyberpunk_Cop_Data_PADD/Cyberpunk_Cop_Data_PADD/Program.cs
@@ -9,6 +9,16 @@ namespace Cyberpunk_Cop_Data_PADD
         private const int middleRange = 5000;
         private const int upperRange = 10000;
 
+        private string[] name = new string[2];
+        private string[] surname = new string[2];
+        private string[] wanted = new string[3];
+        private string[] bounty = new string[2];
+        private string[] misdemeanors = new string[2];
+        private string[] felonies = new string[2];
+
+
+
+
         private static int RandomSleepTimer(int int1, int int2) //generates a random value between two ints
         {
             var rnd = new Random();
@@ -18,6 +28,60 @@ namespace Cyberpunk_Cop_Data_PADD
             return sleepTime;
         }
 
+        public static void BootLoading()
+        {
+            Console.WriteLine("System Starting, Please Wait....."); // prints the boot loading message
+            Console.WriteLine("Loading Memory...... 64kb");
+            Console.WriteLine("Loading Memory...... 128kb");
+            Thread.Sleep(RandomSleepTimer(lowerRange, upperRange));
+            Console.WriteLine("Loading Memory...... 256kb");
+            Console.WriteLine("Loading Memory...... 512kb");
+            Thread.Sleep(RandomSleepTimer(lowerRange, upperRange));
+            Console.WriteLine("Memory Loaded");
+            Console.WriteLine("Booting OS, Please Wait.....");
+            Thread.Sleep(RandomSleepTimer(lowerRange, upperRange));
+            Console.WriteLine(" ______          ");
+            Console.WriteLine("|__||__|     //||");
+            Console.WriteLine("   ||       // ||");
+            Console.WriteLine("   ||      //  ||");
+            Console.WriteLine("   ||     //   ||");
+            Console.WriteLine("   ||    //    ||");
+            Console.WriteLine("   ||   //_____||");
+            Console.WriteLine("   ||  //______||");
+            Console.WriteLine("   || //       ||");
+            Console.WriteLine("   ||//        ||");
+            Thread.Sleep(RandomSleepTimer(lowerRange, middleRange));
+            Console.WriteLine("TRIANGLE CORPS PRESENTS");
+            Thread.Sleep(RandomSleepTimer(lowerRange, middleRange));
+            Console.WriteLine("Right Angle OS");
+            Thread.Sleep(RandomSleepTimer(lowerRange, middleRange));
+            Console.WriteLine("v 0.01.23.453");
+        }
+
+        public static string UserLogin()
+        {
+            Console.WriteLine("Please Enter User ID:");
+            string userId = Console.ReadLine();
+            Console.WriteLine("Please Enter Password:");
+            string userPassword = Console.ReadLine();
+            Console.WriteLine("Connecting to GlobaNet Services.....");
+            Thread.Sleep(RandomSleepTimer(middleRange, upperRange));
+            Console.WriteLine("Credentials Accepted, Welcome User " + userId);
+            Console.WriteLine("Loading Database, Please Wait.....");
+            Thread.Sleep(RandomSleepTimer(middleRange, upperRange));
+            Console.WriteLine("Loading Ticketing and Informational Database (TID)");
+            Thread.Sleep(RandomSleepTimer(middleRange, upperRange));
+            Console.WriteLine("TID Loaded");
+            Console.WriteLine("User ID: " + userId);
+            Console.Write("Enter 9 character alphanumeric ID Number: ");
+            string citizenID = Console.ReadLine();
+            Console.WriteLine("Citizen ID: " + citizenID);
+            Console.WriteLine("Loading, Please Wait.....");
+            Thread.Sleep(RandomSleepTimer(middleRange, upperRange));
+            Console.WriteLine("ID Found, Loading......");
+            Thread.Sleep(RandomSleepTimer(middleRange, upperRange));
+            return (userId, userPassword, citizenID);
+        }
 
 
 
@@ -61,62 +125,12 @@ namespace Cyberpunk_Cop_Data_PADD
              * 
              * ---------------------------------------------------------
              */
+            string citizenID = "";
 
+            BootLoading();
 
+            UserLogin();
 
-            //Begin Boot Sequence
-            //---------------------------------------------------------
-            Console.WriteLine("System Starting, Please Wait....."); // prints the boot loading message
-            Console.WriteLine("Loading Memory...... 64kb");
-            Console.WriteLine("Loading Memory...... 128kb");
-            Thread.Sleep(RandomSleepTimer(lowerRange, upperRange));
-            Console.WriteLine("Loading Memory...... 256kb");
-            Console.WriteLine("Loading Memory...... 512kb");
-            Thread.Sleep(RandomSleepTimer(lowerRange, upperRange));
-            Console.WriteLine("Memory Loaded");
-            Console.WriteLine("Booting OS, Please Wait.....");
-            Thread.Sleep(RandomSleepTimer(lowerRange, upperRange));
-            Console.WriteLine(" ______          ");
-            Console.WriteLine("|__||__|     //||");
-            Console.WriteLine("   ||       // ||");
-            Console.WriteLine("   ||      //  ||");
-            Console.WriteLine("   ||     //   ||");
-            Console.WriteLine("   ||    //    ||");
-            Console.WriteLine("   ||   //_____||");
-            Console.WriteLine("   ||  //______||");
-            Console.WriteLine("   || //       ||");
-            Console.WriteLine("   ||//        ||");
-            Thread.Sleep(RandomSleepTimer(lowerRange, middleRange));
-            Console.WriteLine("TRIANGLE CORPS PRESENTS");
-            Thread.Sleep(RandomSleepTimer(lowerRange, middleRange));
-            Console.WriteLine("Right Angle OS");
-            Thread.Sleep(RandomSleepTimer(lowerRange, middleRange));
-            Console.WriteLine("v 0.01.23.453");
-            //end of boot sequence
-            //---------------------------------------------------------
-
-            //begin usage
-            //---------------------------------------------------------
-            Console.WriteLine("Please Enter User ID:");
-            string userId = Console.ReadLine();
-            Console.WriteLine("Please Enter Password:");
-            string userPassword = Console.ReadLine();
-            Console.WriteLine("Connecting to GlobaNet Services.....");
-            Thread.Sleep(RandomSleepTimer(middleRange, upperRange));
-            Console.WriteLine("Credentials Accepted, Welcome User " + userId);
-            Console.WriteLine("Loading Database, Please Wait.....");
-            Thread.Sleep(RandomSleepTimer(middleRange, upperRange));
-            Console.WriteLine("Loading Ticketing and Informational Database (TID)");
-            Thread.Sleep(RandomSleepTimer(middleRange, upperRange));
-            Console.WriteLine("TID Loaded");
-            Console.WriteLine("User ID: " + userId);
-            Console.Write("Enter 9 character alphanumeric ID Number: ");
-            string citizenID = Console.ReadLine();
-            Console.WriteLine("Citizen ID: " + citizenID);
-            Console.WriteLine("Loading, Please Wait.....");
-            Thread.Sleep(RandomSleepTimer(middleRange, upperRange));
-            Console.WriteLine("ID Found, Loading......");
-            Thread.Sleep(RandomSleepTimer(middleRange, upperRange));
 
             string citizenName = "Jefferey"; //placeholder for a variable that references an index of names
             string citizenSurname = "Colmbs"; //place holder for a variable that references an index of surnames
